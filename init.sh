5dc979fcffbf2139acd8ce26f2bb341d2ae71d2d
Inna Semenova <inna.semenova@gmail.com>
2017-02-10 15:38:23 +0100

    add django project "ask"

--- init.sh
+++ init.sh
@@ -2,4 +2,5 @@
 sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
 sudo ln -sf /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/test
+sudo ln -sf /home/box/web/etc/gunicorn-django.conf /etc/gunicorn.d/test-django
 sudo /etc/init.d/gunicorn restart
