a3b527c38338dec38dceef52feeca55eaf88ab73
Inna Semenova <inna.semenova@gmail.com>
2017-02-22 16:03:26 +0100

    one more variant

--- ask/ask/settings.py
+++ ask/ask/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '!u92@lluwn*h2ub#9yhd&3llea_+499@@o+9xlyrt@-jon-_w_'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['0.0.0.0']
 
 
 # Application definition
